d4c3804a62da2cfce33c5e28a4101c282561a3e4
Zecky-Dev <l2021012041@ogr.sdu.edu.tr>
2023-12-28 12:50:52 +0300

    fix

--- app.js
+++ app.js
@@ -21,4 +21,12 @@ app.use("/api", apiRouter);
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
 
+app.use((req,res,next)=>{
+    res.setHeader('Access-Control-Allow-Origin','*');
+    res.setHeader('Access-Control-Allow-Methods','GET,POST,PUT,PATCH,DELETE');
+    res.setHeader('Access-Control-Allow-Methods','Content-Type','Authorization');
+    next(); 
+})
+
+
 module.exports = app;
